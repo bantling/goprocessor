a1b5df28b188f7a0d8dba271556660c62f1f4044
Greg Hall <greghall@bantling.me>
2020-12-22 16:07:13 -0400

    Improved bit fiddling logic

--- pkg/proc/registers.go
+++ pkg/proc/registers.go
@@ -11,29 +11,50 @@ const (
 	// DefaultSP is the default stack pointer
 	DefaultSP uint16 = 0xFFFF
 
-	// STCarry is ST filter for carry flag
-	STCarry uint32 = 0x80000000
+	// STCarrySet is ST filter for setting carry flag
+	STCarrySet uint32 = 0x80000000
 
-	// STOverflow is ST filter for overflow flag
-	STOverflow uint32 = 0x40000000
+	// STCarryClear is ST filter for clearing carry flag
+	STCarryClear uint32 = 0xFFFFFFFF - STCarrySet
 
-	// STZero is ST filter for zero flag
-	STZero uint32 = 0x20000000
+	// STOverflowSet is ST filter for setting overflow flag
+	STOverflowSet uint32 = 0x40000000
 
-	// STNegative is ST filter for negative flag
-	STNegative uint32 = 0x10000000
+	// STOverflowClear is ST filter for clearing overflow flag
+	STOverflowClear uint32 = 0xFFFFFFFF - STOverflowSet
 
-	// STAddress is ST filter for address mode
-	STAddress uint32 = 0x0E000000
+	// STZeroSet is ST filter for setting zero flag
+	STZeroSet uint32 = 0x20000000
+
+	// STZeroClear is ST filter for clearing zero flag
+	STZeroClear uint32 = 0xFFFFFFFF - STZeroSet
+
+	// STNegativeSet is ST filter for setting negative flag
+	STNegativeSet uint32 = 0x10000000
+
+	// STNegativeClear is ST filter for clearing negative flag
+	STNegativeClear uint32 = 0xFFFFFFFF - STNegativeSet
+
+	// STAddressRead is ST filter for reading address mode
+	STAddressRead uint32 = 0x0E000000
+
+	// STAddressSet is ST filter for setting address mode
+	STAddressSet uint32 = 0xFFFFFFFF - STAddressRead
 
 	// STAddressShift is ST shift for address
 	STAddressShift int = 25
 
-	// STInterruptDisable is ST filter for interrupt disable
-	STInterruptDisable uint32 = 0x01000000
+	// STInterruptDisableSet is ST filter for setting interrupt disable
+	STInterruptDisableSet uint32 = 0x01000000
 
-	// STRegister is ST filter for register
-	STRegister uint32 = 0x00C00000
+	// STInterruptDisableClear is ST filter for clearing interrupt disable
+	STInterruptDisableClear uint32 = 0xFFFFFFFF - STInterruptDisableSet
+
+	// STRegisterRead is ST filter for reading register mode
+	STRegisterRead uint32 = 0x00C00000
+
+	// STRegisterSet is ST filter for setting register mode
+	STRegisterSet uint32 = 0xFFFFFFFF - STRegisterRead
 
 	// STRegisterShift is the ST shift for register
 	STRegisterShift int = 22
@@ -41,29 +62,47 @@ const (
 	// STPointerRegisterSet is ST filter for pointer register set
 	STPointerRegisterSet uint32 = 0x00200000
 
+	// STPointerRegisterClear is ST filter for pointer register clear
+	STPointerRegisterClear uint32 = 0xFFFFFFFF - STPointerRegisterSet
+
 	// STCounterRegisterSet is ST filter for counter register set
 	STCounterRegisterSet uint32 = 0x00100000
 
-	// STOperand is ST filter for operand size
-	STOperand uint32 = 0x000C0000
+	// STCounterRegisterClear is ST filter for counter register clear
+	STCounterRegisterClear uint32 = 0xFFFFFFFF - STCounterRegisterSet
+
+	// STOperandRead is ST filter for reading operand size
+	STOperandRead uint32 = 0x000C0000
+
+	// STOperandSet is ST filter for setting operand size
+	STOperandSet uint32 = 0xFFFFFFFF - STOperandRead
 
 	// STOperandShift is the ST shift for operand
 	STOperandShift int = 18
 
-	// STMath is ST filter for math mode
-	STMath uint32 = 0x00030000
+	// STMathRead is ST filter for reading math mode
+	STMathRead uint32 = 0x00030000
+
+	// STMathSet is ST filter for setting math mode
+	STMathSet uint32 = 0xFFFFFFFF - STMathRead
 
 	// STMathShift is the ST shift for math mode
 	STMathShift int = 16
 
-	// STSystem is ST filter for system bits
-	STSystem uint32 = 0x0000FF00
+	// STSystemRead is ST filter for reading system bits
+	STSystemRead uint32 = 0x0000FF00
+
+	// STSystemSet is ST filter for setting system bits
+	STSystemSet uint32 = 0xFFFFFFFF - STSystemRead
 
 	// STSystemShift is the ST shift for system bits
 	STSystemShift int = 8
 
-	// STUser is ST filter for user bits
-	STUser uint32 = 0x000000FF
+	// STUserRead is ST filter for reading user bits
+	STUserRead uint32 = 0x000000FF
+
+	// STUserSet is ST filter for setting user bits
+	STUserSet uint32 = 0xFFFFFFFF - STUserRead
 )
 
 // Address modes
@@ -123,101 +162,101 @@ type StatusRegister uint32
 
 // Carry returns true if the carry flag is set
 func (st StatusRegister) Carry() bool {
-	return (uint32(st) & STCarry) == STCarry
+	return (uint32(st) & STCarrySet) == STCarrySet
 }
 
 // SetCarry sets the carry flag
 func (st *StatusRegister) SetCarry() {
-	*st |= StatusRegister(STCarry)
+	*st |= StatusRegister(STCarrySet)
 }
 
 // ClearCarry clears the carry flag
 func (st *StatusRegister) ClearCarry() {
-	*st = StatusRegister((uint32(*st) | STCarry) - STCarry)
+	*st &= StatusRegister(STCarryClear)
 }
 
 // Overflow returns true if the overflow flag is set
 func (st StatusRegister) Overflow() bool {
-	return (uint32(st) & STOverflow) == STOverflow
+	return (uint32(st) & STOverflowSet) == STOverflowSet
 }
 
 // SetOverflow sets the overflow flag
 func (st *StatusRegister) SetOverflow() {
-	*st |= StatusRegister(STOverflow)
+	*st |= StatusRegister(STOverflowSet)
 }
 
 // ClearOverflow clears the overflow flag
 func (st *StatusRegister) ClearOverflow() {
-	*st = StatusRegister((uint32(*st) | STOverflow) - STOverflow)
+	*st &= StatusRegister(STOverflowClear)
 }
 
 // Zero returns true if the overflow flag is set
 func (st StatusRegister) Zero() bool {
-	return (uint32(st) & STZero) == STZero
+	return (uint32(st) & STZeroSet) == STZeroSet
 }
 
 // SetZero sets the overflow flag
 func (st *StatusRegister) SetZero() {
-	*st |= StatusRegister(STZero)
+	*st |= StatusRegister(STZeroSet)
 }
 
 // ClearZero clears the overflow flag
 func (st *StatusRegister) ClearZero() {
-	*st = StatusRegister((uint32(*st) | STZero) - STZero)
+	*st &= StatusRegister(STZeroClear)
 }
 
 // Negative returns true if the negative flag is set
 func (st StatusRegister) Negative() bool {
-	return (uint32(st) & STNegative) == STNegative
+	return (uint32(st) & STNegativeSet) == STNegativeSet
 }
 
 // SetNegative sets the negative flag
 func (st *StatusRegister) SetNegative() {
-	*st |= StatusRegister(STNegative)
+	*st |= StatusRegister(STNegativeSet)
 }
 
 // ClearNegative clears the negative flag
 func (st *StatusRegister) ClearNegative() {
-	*st = StatusRegister((uint32(*st) | STNegative) - STNegative)
+	*st &= StatusRegister(STNegativeClear)
 }
 
 // AddressMode returns the address mode
 func (st StatusRegister) AddressMode() uint8 {
-	return uint8((uint32(st) & STAddress) >> STAddressShift)
+	return uint8((uint32(st) & STAddressRead) >> STAddressShift)
 }
 
 // SetAddressMode sets the address mode
 func (st *StatusRegister) SetAddressMode(am uint8) {
 	gofuncs.PanicBM(am <= PtrPtrIxOfs, STAddressModeErr)
 
-	*st = StatusRegister(((uint32(*st) | STAddress) - STAddress) + (uint32(am) << STAddressShift))
+	*st = StatusRegister((uint32(*st) & STAddressSet) + (uint32(am) << STAddressShift))
 }
 
 // InterruptDisable returns true if the interrupt disable flag is set
 func (st StatusRegister) InterruptDisable() bool {
-	return (uint32(st) & STInterruptDisable) == STInterruptDisable
+	return (uint32(st) & STInterruptDisableSet) == STInterruptDisableSet
 }
 
 // SetInterruptDisable sets the interrupt disable flag
 func (st *StatusRegister) SetInterruptDisable() {
-	*st |= StatusRegister(STInterruptDisable)
+	*st |= StatusRegister(STInterruptDisableSet)
 }
 
 // ClearInterruptDisable clears the interrupt disable flag
 func (st *StatusRegister) ClearInterruptDisable() {
-	*st = StatusRegister((uint32(*st) | STInterruptDisable) - STInterruptDisable)
+	*st = StatusRegister(STInterruptDisableClear)
 }
 
 // RegisterMode returns the register mode
 func (st StatusRegister) RegisterMode() uint8 {
-	return uint8((uint32(st) & STRegister) >> STRegisterShift)
+	return uint8((uint32(st) & STRegisterRead) >> STRegisterShift)
 }
 
 // SetRegisterMode sets the register mode
 func (st *StatusRegister) SetRegisterMode(rm uint8) {
 	gofuncs.PanicBM(rm <= Operand64, STRegisterErr)
 
-	*st = StatusRegister(((uint32(*st) | STRegister) - STRegister) + (uint32(rm) << STRegisterShift))
+	*st = StatusRegister((uint32(*st) & STRegisterSet) + (uint32(rm) << STRegisterShift))
 }
 
 // PointerRegisterSet0 returns true if pointer register set 0 is selected
@@ -232,7 +271,7 @@ func (st StatusRegister) PointerRegisterSet1() bool {
 
 // SetPointerRegisterSet0 selects pointer register set 0
 func (st *StatusRegister) SetPointerRegisterSet0() {
-	*st = StatusRegister((uint32(*st) | STPointerRegisterSet) - STPointerRegisterSet)
+	*st &= StatusRegister(STPointerRegisterClear)
 }
 
 // SetPointerRegisterSet1 selects pointer register set 1
@@ -252,7 +291,7 @@ func (st StatusRegister) CounterRegisterSet1() bool {
 
 // SetCounterRegisterSet0 selects counter register set 0
 func (st *StatusRegister) SetCounterRegisterSet0() {
-	*st = StatusRegister((uint32(*st) | STCounterRegisterSet) - STCounterRegisterSet)
+	*st &= StatusRegister(STCounterRegisterClear)
 }
 
 // SetCounterRegisterSet1 selects counter register set 1
@@ -262,46 +301,46 @@ func (st *StatusRegister) SetCounterRegisterSet1() {
 
 // OperandSize returns the operand size
 func (st StatusRegister) OperandSize() uint8 {
-	return uint8((uint32(st) & STOperand) >> STOperandShift)
+	return uint8((uint32(st) & STOperandRead) >> STOperandShift)
 }
 
 // SetOperandSize sets the operand size
 func (st *StatusRegister) SetOperandSize(om uint8) {
 	gofuncs.PanicBM(om <= Operand64, STOperandModeErr)
 
-	*st = StatusRegister(((uint32(*st) | STOperand) - STOperand) + (uint32(om) << STOperandShift))
+	*st = StatusRegister((uint32(*st) & STOperandSet) + (uint32(om) << STOperandShift))
 }
 
 // MathMode returns the math mode
 func (st StatusRegister) MathMode() uint8 {
-	return uint8((uint32(st) & STMath) >> STMathShift)
+	return uint8((uint32(st) & STMathRead) >> STMathShift)
 }
 
 // SetMathMode sets the math mode
 func (st *StatusRegister) SetMathMode(mm uint8) {
 	gofuncs.PanicBM(mm <= MathFloat, STMathModeErr)
 
-	*st = StatusRegister(((uint32(*st) | STMath) - STMath) + (uint32(mm) << STMathShift))
+	*st = StatusRegister((uint32(*st) & STMathSet) + (uint32(mm) << STMathShift))
 }
 
 // System returns the system defined ST bits
 func (st StatusRegister) System() uint8 {
-	return uint8((uint32(st) & STSystem) >> STSystemShift)
+	return uint8((uint32(st) & STSystemRead) >> STSystemShift)
 }
 
 // SetSystem sets the system defined ST bits
 func (st *StatusRegister) SetSystem(sb uint8) {
-	*st = StatusRegister(((uint32(*st) | STSystem) - STSystem) + (uint32(sb) << STSystemShift))
+	*st = StatusRegister((uint32(*st) & STSystemSet) + (uint32(sb) << STSystemShift))
 }
 
 // User returns the user defined ST bits
 func (st StatusRegister) User() uint8 {
-	return uint8(uint32(st) & STUser)
+	return uint8(uint32(st) & STUserRead)
 }
 
 // SetUser sets the user defined ST bits
 func (st *StatusRegister) SetUser(sb uint8) {
-	*st = StatusRegister(((uint32(*st) | STUser) - STUser) + uint32(sb))
+	*st = StatusRegister((uint32(*st) & STUserSet) + uint32(sb))
 }
 
 // Registers contains the processor registers
